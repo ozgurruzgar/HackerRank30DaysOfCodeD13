c3098c9862f1e365b34836e768abf099844c8e29
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-09 18:57:28 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD13/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank30DaysOfCodeD13
+{
+    abstract class Book
+    {
+
+        protected String title;
+        protected String author;
+
+        public Book(String t, String a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+
+
+    }
+    //Write MyBook Class ----  Kendi MyBook Sınıfını Yaz
+    class MyBook : Book
+    {
+        private int price;
+        public MyBook(string title, string author, int myPrice) : base(title, author)
+        {
+            price = myPrice;
+        }
+        public override void display()
+        {
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Price: " + price);
+        }
+    }
+
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
+    }
+}
